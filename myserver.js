83a7aa771d1d905c5151293111f2f774eeeaad74
Hung <phanbahungtnvn@gmail.com>
2020-02-27 15:56:07 +0700

    new

--- myserver.js
+++ myserver.js
@@ -7,9 +7,17 @@ var port = process.env.PORT || 3000;
 var publicDir = require('path').join(__dirname, '/public');
 app.use(express.static(publicDir));
 
+var bodyParser = require('body-Parser');
+app.use(bodyParser.urlencoded({ extended: true }));
 // trang mac dinh
 app.get("/", (req, res) => {
     res.render('index');
 })
-
+app.get('/form', (req, res) => {
+    res.render('form');
+})
+app.post('/saveForm', (req, res) => {
+    const yourName = req.body.yourName;
+    res.render('confirm', { name: yourName })
+})
 var server = app.listen(port, function() {});
