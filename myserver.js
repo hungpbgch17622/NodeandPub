bad5132c35f7e4c8bc14b0dec37af82e4263cb7e
Hung <phanbahungtnvn@gmail.com>
2020-03-10 13:44:44 +0700

    a

--- myserver.js
+++ myserver.js
@@ -3,17 +3,17 @@ var app = express();
 
 app.set('view engine', 'jade');
 var port = process.env.PORT || 3000;
-// them file public
+
 var publicDir = require('path').join(__dirname, '/public');
 app.use(express.static(publicDir));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// trang mac dinh
-app.get("/", (req, res) => {
+
+app.get("/", function(req, res) {
     res.render('loginpage');
 })
-app.post('/HomePage', (req, res) => {
+app.post('/HomePage', function(req, res) {
     res.render('homepage');
 })
 var server = app.listen(port, function() {});
